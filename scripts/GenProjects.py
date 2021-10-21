0005a270526da16457876d551050e30777c0ad51
Jose <zelico98@gmail.com>
2021-10-21 13:24:44 +0100

    GenProjects for Linux

--- scripts/GenProjects.py
+++ scripts/GenProjects.py
@@ -6,7 +6,6 @@ platform = pl.system()
 os.chdir("..")
 if(platform == "Windows"):
     os.system("premake5.exe vs2019")
+    os.system("PAUSE")
 elif(platform == "Linux"):
-    os.system("premake5 gmake2")
-
-os.system("PAUSE")
+    os.system("./premake5 gmake2")
